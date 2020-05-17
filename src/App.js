d8b12f04ccdbf70888d915ce25565d8b7f14574e
Spencer <cotton2_24@yahoo.com>
2020-05-17 16:08:07 -0600

    Made player names more readable in CSV file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as d3 from 'd3';
+import importedCsvData from './players.csv';
+
 
 const App = () => {
+  
+  
+
+
+  d3.csv(importedCsvData, function(data){
+    
+    console.log(data);
+});
+    
+  
   return (
     <div></div>
 
