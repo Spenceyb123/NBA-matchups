9bd13fd61fde0e07cbf5124d139c4685c4bb25d0
Spencer <cotton2_24@yahoo.com>
2020-05-17 22:12:03 -0600

    before messing with players specifics

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import importedCsvData from './players.csv';
 
 const App = () => {
   
-  
 
 
-  d3.csv(importedCsvData, function(data){
-    
-    console.log(data);
-});
-    
+  d3.csv(importedCsvData).then(function(data) {
+    data.forEach(function(d) {
+      d.TRB = +d.TRB;
+      d.AST = +d.AST;
+      d.STL = +d.STL;
+      d.BLK = +d.BLK;
+      d.TOV = +d.TOV;
+      d.PTS = +d.PTS;
+      d.TSP = +d.TSP;
+
+    }) 
+    console.log(data[20]);
+  });
   
   return (
     <div></div>
@@ -22,3 +29,5 @@ const App = () => {
 }
 
 export default App;
+
+
