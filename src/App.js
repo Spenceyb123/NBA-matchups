899ee0a82e4f0008bf265429b9c6ef9ae3596151
Spencer <cotton2_24@yahoo.com>
2020-05-21 21:31:40 -0600

    figured out how to merge Images array into data array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+// import $ from 'jquery';
 import * as d3 from 'd3';
-import importedCsvData from './players.csv';
+import importedCsvData from './assets/players.csv';
+import Images from'./Images';
+// import MJ from './assets/images/jordan91.jpg';
 
 
 const App = () => {
-  
-
+  let playersWithImages = [];
 
   d3.csv(importedCsvData).then(function(data) {
     data.forEach(function(d) {
@@ -17,13 +19,46 @@ const App = () => {
       d.TOV = +d.TOV;
       d.PTS = +d.PTS;
       d.TSP = +d.TSP;
+
+      
+      // let finishedWithImages = { ...d, ...Images };
+      // console.log(finishedWithImages);
+      // $.extend(d, Images);
+     
     }) 
-    console.log(data[0]);
+    // console.log(data); data like expected, hell ya
+    
+      for (var i = 0; i < data.length; i++) {
+        for (var y = 0; y < Images.length; y++) {
+          if (data[i].Rk == Images[y].id) {
+            Object.assign(data[i], Images[y]); //merges 2nd param into first
+          }
+        }
+      }
+     playersWithImages.push(data); // so data can be accessed outside of above function
+     console.log(playersWithImages);
+     
+     
+      
+     
+          
+    
   });
+
+  console.log(playersWithImages);
+  
+
+
+
+
+  
   
   return (
-    <div></div>
-
+    <div> 
+      {/* {Images.map(({id, src, alt}) => <img key={id} src={src} alt={alt} />)} */}
+      <img src='' alt = "" />
+    </div>
+    
   )
 }
 
---
+++ src/Images.js
@@ -0,0 +1,8 @@
+// import React from 'react';
+
+const Images = [
+    { id: 1, src : './pics/jordan95.jpg', alt : "Michael Jordan 1995-96"},
+    { id: 2, src : './pics/jordan91.jpg', alt : "Michael Jordan 1990-91"}
+]
+
+export default Images;
