8ce0c2965c5e3e3ab5d556121d5847cd9ee5a7b7
Spencer <cotton2_24@yahoo.com>
2020-09-21 21:24:10 -0600

    bug fix

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -96,7 +96,7 @@ $(function(e){
             if(randomStat === 0 && statsArrayRandom[randomStat] > statsArrayRandomTwo[randomStat]) {
                 (() => {
                 
-                    let ppg =  document.querySelectorAll('.stats-before-hover.PPG'); // not iterable?
+                    let ppg =  document.getElementsByClassName(this.props.children[2].props.children[0].props.className); 
                     
                     for (const p of ppg) {
                         p.classList.add("animation");
